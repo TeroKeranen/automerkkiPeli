adeb334b1b6df548a3156dfecccd48ed8af9df06
Tero Keränen <tero.keranen@edu.taitotalo.fi>
2022-04-03 20:34:47 +0300

    Html lisätty Correct answer osio ja javascriptiin lisätty laskin montako arvausta mennyt oikein ja correctAnswer function

--- java.js
+++ java.js
@@ -5,33 +5,47 @@ img = document.querySelector("img"); // valitaan img element
 randomImage = "kuvat/auto" + randomNum + ".png"; // luodaan polku kuvalle randomNumeron mukaan
 img.setAttribute("src", randomImage); // Asetetaan kuva näytölle
 
-///////////////////////////////////////////////////////////
+oikeinArvattu = 0;
+ArvattuOikein = document.querySelector(".correct-answers");
 
+highScoreSpan = document.querySelector(".highScore-value"); // Valitaan highscore
+highScore = 0;
+
+//////////////////////////////////////////////////////////
+
+function correctAnswers() {
+  oikeinArvattu++;
+  ArvattuOikein.textContent = oikeinArvattu;
+}
+
+///////////////////Luodaan testi function joka toimii onclickin kauttta //////////////////////////////////
 function testi() {
+  ///// arvaus function alkaa ////// functio arvaus. Jos vastaus on oikein niin tulee uusi kuva esille
   function arvaus() {
-    // functio arvaus. Jos vastaus on oikein niin tulee uusi kuva esille
+    ////// Random functio alkaa /////// functio random. Toimii vain silloin kun function arvaus on true.
     function random() {
-      // functio random. Toimii vain silloin kun function arvaus on true.
       randomNum = Math.trunc(Math.random() * +6); //function randomin sisälle luodaan uusi random numero.
       img = document.querySelector("img"); // valitaan taas img elemenet
       randomImage = "kuvat/auto" + randomNum + ".png"; // Tehdään taas kuvan polku
       img.setAttribute("src", randomImage); // Asetetaan uusi kuva
+      correctAnswers.textContent = oikeinArvattu;
     }
+    ////// Random functio loppuu ///////
 
     if (arrayNum === randomNum) {
-      console.log("oikein");
+      correctAnswers();
       random();
 
-      img.setAttribute("src", randomImage);
-      console.log(`arvauksesi : ${arvaa}`);
-      console.log(`random numero : ${randomNum}`);
-      console.log(`array num numero : ${arrayNum}`);
+      if (oikeinArvattu > highScore) {
+        highScore = oikeinArvattu;
+      }
     } else if (arrayNum !== randomNum) {
       console.log("eiii");
     }
   }
+  ///// arvaus function loppuu ////
 
-  // Luokaan objecti
+  // Luokaan objecti johon sijoitetaan autot ja annetaan avaimet niille
   const autot = {
     tesla: 0,
     bmw: 1,
@@ -46,3 +60,4 @@ function testi() {
   let objectKey = Object.keys(autot)[arrayNum]; // kertoo array2 numeron vastauksen --- Ei tässä tapauksessa tarpeellinen mutta harjoituksen kannalta oli hyvä tajuta tämä toiminto.
   arvaus(); // Arvaus functio joka sisältää random function
 }
+////////////////////////////////////////////////////////
