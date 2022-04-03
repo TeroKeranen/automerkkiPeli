e0de285ae56f6c8808a3069c8212e8be701fea2f
Tero Keränen <tero.keranen@edu.taitotalo.fi>
2022-04-03 19:43:39 +0300

    Addin photo folder

---
+++ java.js
@@ -0,0 +1,8 @@
+/////////// Random kuva sivulle /////////////////////
+
+randomNum = Math.trunc(Math.random() * +6); // Luodaan random numero
+img = document.querySelector("img"); // valitaan img element
+randomImage = "kuvat/auto" + randomNum + ".png"; // luodaan polku kuvalle randomNumeron mukaan
+img.setAttribute("src", randomImage); // Asetetaan kuva näytölle
+
+//////////////////////////////////////////////////////////
